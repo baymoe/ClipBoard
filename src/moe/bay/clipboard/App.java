4adcc931a8097b7fe7b9c157f54d29c3419322b8
Bailey <robin.riezebos@gmail.com>
2020-01-19 14:32:47 +0100

    Create App.java
    main class with properties loader
    creates new clipboard on startup

---
+++ src/moe/bay/clipboard/App.java
@@ -0,0 +1,44 @@
+package moe.bay.clipboard;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class App {
+    public static void main(String[] args) throws IOException, SQLException {
+
+        final File propFile = new File("clipboard.conf");
+
+        if (!propFile.exists()) {
+            final String contents =
+                    "discord-token-\n"
+                            + "db-host=\n"
+                            + "db-port=\n"
+                            + "db-user=\n"
+                            + "db-pass=\n"
+                            + "debug=true";
+            FileUtils.write(propFile, contents, "UTF-8");
+            System.out.println("Please set up the configuration file.");
+            return;
+        }
+
+        final Properties properties = new Properties();
+
+        try (InputStream stream = new FileInputStream(propFile)) {
+            properties.load(stream);
+        }
+
+        try {
+            new ClipBoard(properties);
+        } catch (SQLException e) {
+            System.out.println("Could not connect to database, stopping ClipBoard.");
+            e.printStackTrace();
+            return;
+        }
+    }
+}
